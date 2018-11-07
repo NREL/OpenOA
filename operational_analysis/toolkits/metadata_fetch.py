da9281c67755296a1dd1c0ca414153880e7d4eaa
Optis <moptis@nrel.gov>
2018-11-07 12:20:47 -0700

    fixed indents, some updates to code

--- operational_analysis/toolkits/metadata_fetch.py
+++ operational_analysis/toolkits/metadata_fetch.py
@@ -24,48 +24,50 @@ def fetch_eia(api_key, plant_id, file_path):
 
     """
 	
-	# EIA monthly energy production data
+    # EIA metadata
 	
-	api = eia.API(api_key) # get data from EIA
-	
-    series_search_m = api.data_by_series(series='ELEC.PLANT.GEN.%s-ALL-ALL.M' % plant_id)
-	eia_monthly = pd.DataFrame(series_search_m) # net monthly energy generation of wind farm in MWh
-	eia_monthly.columns = ['EIA_monthly_MWh'] # rename column
-	eia_monthly = eia_monthly.set_index(pd.DatetimeIndex(eia_monthly.index)) # convert to DatetimeIndex
-	
-	# EIA metadata
-	
-	plant_var_list = ['City', 'Latitude', 'Longitude', 'Balancing Authority Name', 
+    plant_var_list = ['City', 'Latitude', 'Longitude', 'Balancing Authority Name', 
 					  'Transmission or Distribution System Owner']
 
-	wind_var_list = ['Utility Name', 'Plant Name', 'State', 'County', 'Nameplate Capacity (MW)', 
+    wind_var_list = ['Utility Name', 'Plant Name', 'State', 'County', 'Nameplate Capacity (MW)', 
 					 'Operating Month', 'Operating Year', 'Number of Turbines', 
 					 'Predominant Turbine Manufacturer', 'Predominant Turbine Model Number', 
 					 'Turbine Hub Height (Feet)']
 	
-	def meta_dic_fn(file, sheet, var_list): 
+    def meta_dic_fn(metafile, sheet, var_list): 
     
-		all_plant = pd.read_excel(file_path+file, sheet_name=sheet, skiprows=1)
+        all_plant = pd.read_excel(file_path+metafile, sheet_name=sheet, skiprows=1)
 	
-		eia_plant = all_plant.loc[all_plant['Plant Code']==np.int(plant_id)] # specific wind farm 
-		eia_info = eia_plant.loc[:, lambda eia_plant:var_list] # select columns
-		
-		eia_info = eia_info.reset_index(drop=True) # reset index to 0
-		eia_dic = eia_info.T.to_dict() # convert to dictionary
-		out_dic = eia_dic[0] # remove extra level of dictionary, "0" in this case
-		
-		return(out_dic)
+        eia_plant = all_plant.loc[all_plant['Plant Code']==np.int(plant_id)] # specific wind farm
+        
+        if (eia_plant.shape[0] == 0): # Couldn't locate EIA ID in database
+            raise Exception('Plant ID not found in EIA database')
+        
+        eia_info = eia_plant.loc[:, lambda eia_plant:var_list] # select column
+        eia_info = eia_info.reset_index(drop=True) # reset index to 0
+        eia_dic = eia_info.T.to_dict() # convert to dictionary
+        out_dic = eia_dic[0] # remove extra level of dictionary, "0" in this case
+        
+        return(out_dic)
 
     # file path with 2017 EIA metadata files
-	plant_dic = meta_dic_fn('2___Plant_Y2017.xlsx', 'Plant', plant_var_list) 
-	wind_dic = meta_dic_fn('3_2_Wind_Y2017.xlsx', 'Operable', wind_var_list)
+    plant_dic = meta_dic_fn('2___Plant_Y2017.xlsx', 'Plant', plant_var_list) 
+    wind_dic = meta_dic_fn('3_2_Wind_Y2017.xlsx', 'Operable', wind_var_list)
 	
-	# convert feet to meter
-	hubheight_meter = unit_conversion.convert_feet_to_meter(wind_dic['Turbine Hub Height (Feet)']) 
-	wind_dic.update({'Turbine Hub Height (m)': hubheight_meter})
-	wind_dic.pop('Turbine Hub Height (Feet)', None) # delete hub height in feet
+	 # convert feet to meter
+    hubheight_meter = np.round(unit_conversion.convert_feet_to_meter(wind_dic['Turbine Hub Height (Feet)']))
+    wind_dic.update({'Turbine Hub Height (m)': hubheight_meter})
+    wind_dic.pop('Turbine Hub Height (Feet)', None) # delete hub height in feet
+    out_dic = plant_dic.copy()
+    out_dic.update(wind_dic) # append dictionary
+	 
+    # EIA monthly energy production data
 	
-	out_dic = plant_dic.copy()
-	out_dic.update(wind_dic) # append dictionary
+    api = eia.API(api_key) # get data from EIA
 	
+    series_search_m = api.data_by_series(series='ELEC.PLANT.GEN.%s-ALL-ALL.M' % plant_id)
+    eia_monthly = pd.DataFrame(series_search_m) # net monthly energy generation of wind farm in MWh
+    eia_monthly.columns = ['eia_monthly_mwh'] # rename column
+    eia_monthly = eia_monthly.set_index(pd.DatetimeIndex(eia_monthly.index)) # convert to DatetimeIndex
+    
     return eia_monthly, out_dic
