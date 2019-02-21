94ca0bd49acb54c63506095cbda64e05029570d7
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2019-02-21 13:23:50 -0700

    fixed errors in plant_schema to resolve joseph's comments

--- operational_analysis/toolkits/power_curve/functions.py
+++ operational_analysis/toolkits/power_curve/functions.py
@@ -1,5 +1,5 @@
-from .parametric_forms import *
-from .parametric_optimize import *
+from .parametric_forms import logistic5param
+from .parametric_optimize import fit_parametric_power_curve, least_squares
 from scipy.optimize import differential_evolution
 from pygam import LinearGAM
 import pandas as pd
