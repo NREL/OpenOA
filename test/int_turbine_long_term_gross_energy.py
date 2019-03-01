1fce09cf1ac7e1e87e4a8f914f0c819b284c865c
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2019-03-01 15:32:59 -0700

    Added fixed output test to turbine_long_term_gross_energy integration test

--- test/int_turbine_long_term_gross_energy.py
+++ test/int_turbine_long_term_gross_energy.py
@@ -1,31 +1,26 @@
 import unittest
 
 import numpy as np
+import numpy.testing as npt
 
 from operational_analysis.methods.turbine_long_term_gross_energy import TurbineLongTermGrossEnergy
 from examples.turbine_analysis.turbine_project import TurbineExampleProject
-
-#import pdb
 
 
 class TestPandasPrufPlantAnalysis(unittest.TestCase):
 
     def setUp(self):
         np.random.seed(42)
-
         # Set up data to use for testing (TurbineExampleProject)
         self.project = TurbineExampleProject('./examples/turbine_analysis/data')
         self.project.prepare()
-
-        #pdb.set_trace()
-
         self.analysis = TurbineLongTermGrossEnergy(self.project)
         self.analysis.run()
 
-
     def test_longterm_gross_energy_results(self):
-        pass
-
+        res = self.analysis._summary_results.values.astype("float")
+        check = np.ones_like(res)*1.35
+        npt.assert_almost_equal(res/1000000, check, decimal=1)
 
     def tearDown(self):
         pass
