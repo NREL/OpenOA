f6a12aaa44365b43f9e0de9f6e61a1257584316b
Mike Optis <mike.optis@nrel.gov>
2019-03-01 15:18:30 -0700

    fixed integration test for turb_gross

--- examples/turbine_analysis/turbine_project.py
+++ examples/turbine_analysis/turbine_project.py
@@ -2,7 +2,7 @@ import pandas as pd
 from operational_analysis.types import PlantData
 import numpy as np
 from operational_analysis.toolkits.unit_conversion import convert_power_to_energy
-
+from operational_analysis.toolkits.met_data_processing import compute_wind_direction
 
 class TurbineExampleProject(PlantData):
     """This class loads data for NREL's GE Turbine into a PlantData object"""
@@ -41,11 +41,14 @@ class TurbineExampleProject(PlantData):
             d["u_ms"] = np.random.random(d.shape[0]) * 15
             d["v_ms"] = np.random.random(d.shape[0]) * 15
             d["windspeed_ms"] = np.sqrt(d["u_ms"]**2 + d["v_ms"]**2)
+            d["winddirection_deg"] = compute_wind_direction(d["u_ms"], d["v_ms"])
             d["rho_kgm-3"] = np.random.random(d.shape[0]) * 20
             return d
-        p._reanalysis._product['merra2'].df = generate_reanal(p.scada.df.index)
-        p._reanalysis._product['erai'].df = generate_reanal(p.scada.df.index)
-        p._reanalysis._product['ncep2'].df = generate_reanal(p.scada.df.index)
+        
+        index_reanal = pd.date_range(start = '1990-01-01', end = '2017-12-31')
+        p._reanalysis._product['merra2'].df = generate_reanal(index_reanal)
+        p._reanalysis._product['erai'].df = generate_reanal(index_reanal)
+        p._reanalysis._product['ncep2'].df = generate_reanal(index_reanal)
 
         # Project is missing energy column
         p.scada.df['energy_kwh'] = convert_power_to_energy(p.scada.df['power_kw'], sample_rate_min=10.0)
