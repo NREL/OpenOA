8878836368969fd637523a785519c9a24b03fd51
Rafael M Mudafort <rafmudaf@gmail.com>
2019-04-28 22:17:01 -0600

    Add missing requirements

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -35,8 +35,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinx.ext.napoleon',
-              'sphinx.ext.todo',
-              'm2r']
+              'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
