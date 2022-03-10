32958999cae557fe17504c4a1f30425daae3102f
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-03-09 18:25:35 -0700

    stashing changes

--- operational_analysis/types/plant.py
+++ operational_analysis/types/plant.py
@@ -1,3 +1,4 @@
+from ctypes import _SimpleCData
 import io
 import json
 import os
@@ -13,86 +14,111 @@ from .reanalysis import ReanalysisData
 import pandas as pd
 
 
-# @dataclass
-# class PlantDataV2:
-#     scada: pd.DataFrame
-#     meter: pd.DataFrame
-#     tower: pd.DataFrame
-#     status: pd.DataFrame
-#     curtail: pd.DataFrame
-#     asset: pd.DataFrame
-#     reanalysis: pd.DataFrame
+schema = openoa.analysis.aep.getSchema()
+schema = {"power_kw":"10T", ...}
 
-#     name: str
-#     version: float = 2
+# No prepare needed
+plant = openoa.plant.from_entr("Bobkitten", schema)
 
-# def validate(plant, schema):
-#     pass
+scada = pd.read_csv('file.csv')
+scada.rename({})
+plant = openoa.plant.from_pandas(scada, schema)
+toolkit.planetos.fetch(plant)
+## plant.validate(schema)
 
-# def from_entr(thrift_server_host:str="localhost",
-#               thrift_server_port:int=10000,
-#               database:str="entr_warehouse",
-#               wind_plant:str="",
-#               aggregation:str="",
-#               date_range:list=None):
-#     """
-#     from_entr
+# Prepare needed
+plant = openoa.plant.from_csv(files, schema_mapper?)
 
-#     Load a PlantData object from data in an entr_warehouse.
+openoa.analysis.aep(plant)
 
-#     Args:
-#         thrift_server_url(str): URL of the Apache Thrift server
-#         database(str): Name of the Hive database
-#         wind_plant(str): Name of the wind plant you'd like to load
-#         aggregation: Not yet implemented
-#         date_range: Not yet implemented
+@dataclass
+class PlantDataV2:
+    scada: pd.DataFrame
+    meter: pd.DataFrame
+    tower: pd.DataFrame
+    status: pd.DataFrame
+    curtail: pd.DataFrame
+    asset: pd.DataFrame
+    reanalysis: pd.DataFrame
+    name: str
+    version: float = 2
 
-#     Returns:
-#         plant(PlantData): An OpenOA PlantData object.
-#     """
-#     from pyhive import hive
+    def __init__(self, schema):
 
-#     conn = hive.Connection(host=thrift_server_host, port=thrift_server_port)
 
-#     scada_query = """SELECT Wind_turbine_name as Wind_turbine_name, 
-#             Date_time as Date_time, 
-#             cast(P_avg as float) as P_avg,
-#             cast(Power_W as float) as Power_W,
-#             cast(Ws_avg as float) as Ws_avg,
-#             Wa_avg as Wa_avg,
-#             Va_avg as Va_avg, 
-#             Ya_avg as Ya_avg, 
-#             Ot_avg as Ot_avg, 
-#             Ba_avg as Ba_avg 
+
+def validate(plant, schema):
+    pass
+
+def from_pandas(col_mapping={}):
+
+def from_csv(col_mapping={}):
+
+
+def from_entr(thrift_server_host:str="localhost",
+              thrift_server_port:int=10000,
+              database:str="entr_warehouse",
+              wind_plant:str="",
+              aggregation:str="",
+              date_range:list=None):
+    """
+    from_entr
+
+    Load a PlantData object from data in an entr_warehouse.
+
+    Args:
+        thrift_server_url(str): URL of the Apache Thrift server
+        database(str): Name of the Hive database
+        wind_plant(str): Name of the wind plant you'd like to load
+        aggregation: Not yet implemented
+        date_range: Not yet implemented
+
+    Returns:
+        plant(PlantData): An OpenOA PlantData object.
+    """
+    from pyhive import hive
+
+    conn = hive.Connection(host=thrift_server_host, port=thrift_server_port)
+
+    scada_query = """SELECT Wind_turbine_name as Wind_turbine_name, 
+            Date_time as Date_time, 
+            cast(P_avg as float) as P_avg,
+            cast(Power_W as float) as Power_W,
+            cast(Ws_avg as float) as Ws_avg,
+            Wa_avg as Wa_avg,
+            Va_avg as Va_avg, 
+            Ya_avg as Ya_avg, 
+            Ot_avg as Ot_avg, 
+            Ba_avg as Ba_avg 
             
-#      FROM entr_warehouse.la_haute_borne_scada_for_openoa
-#     """
+     FROM entr_warehouse.la_haute_borne_scada_for_openoa
+    """
 
-#     plant = PlantDataV2()
+    plant = PlantDataV2()
 
-#     plant.scada.df = pd.read_sql(scada_query, conn)
+    plant.scada.df = pd.read_sql(scada_query, conn)
 
-#     conn.close()
+    conn.close()
 
-#     validate(plant)
+    validate(plant)
 
-#     return plant
+    return plant
 
-# def from_plantdata_v1(plant_v1:PlantData):
-#     plant_v2 = PlantDataV2()
-#     plant_v2.scada = plant_v1.scada._df
-#     plant_v2.asset = plant_v1.asset._df
-#     plant_v2.meter = plant_v1.meter._df
-#     plant_v2.tower = plant_v1.tower._df
-#     plant_v2.status = plant_v1.status._df
-#     plant_v2.curtail = plant_v1.curtail._df
-#     plant_v2.reanalysis = plant_v1.reanalysis._df
+def from_plantdata_v1(plant_v1:PlantData):
+    plant_v2 = PlantDataV2()
+    plant_v2.scada = plant_v1.scada._df
+    plant_v2.asset = plant_v1.asset._df
+    plant_v2.meter = plant_v1.meter._df
+    plant_v2.tower = plant_v1.tower._df
+    plant_v2.status = plant_v1.status._df
+    plant_v2.curtail = plant_v1.curtail._df
+    plant_v2.reanalysis = plant_v1.reanalysis._df
 
-#     # copy any other data members to their new location
+    # copy any other data members to their new location
 
-#     # validate(plant_v2)
+    # validate(plant_v2)
 
-#     return plant_v2
+    return plant_v2
 
 
 
