f7c7dceefbf42625612e4ed30babf18fcf9b52c7
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2022-09-27 11:58:34 -0600

    update ML init and use atts for setup

--- openoa/utils/machine_learning_setup.py
+++ openoa/utils/machine_learning_setup.py
@@ -1,97 +1,113 @@
-import numpy as np
-from sklearn.metrics import r2_score, make_scorer
-from sklearn.model_selection import KFold
-
-
 """
-This module is a library of machine learning algorithms and associated
-hyperparameter ranges suitable for wind energy analysis. This module allows
-for simple implementation of hyperparameter optimization and the application of
-the best hyperparameter combinations for use in the predictive model.
+This module is a library of machine learning algorithms and associated hyperparameter ranges
+suitable for wind energy analysis. This module allow for simple implementation of hyperparameter
+optimization and the application of the best hyperparameter combinations for use in the predictive
+model.
 
-For the most part, this toolkit is effectively a wrapper over SciKit-Learn machine
-learning algorithms that help ensure consistent and appropriate use of the
-machine learning algorithms. Specifically, this toolkit constrains the following:
+For the most part, this toolkit is effectively a wrapper over SciKit-Learn machine learning
+algorithms that help ensure consistent and appropriate use of the machine learning algorithms.
+Specifically, this toolkit constrains the following:
 
     1. The learning algorithms available
 
-    These include extremely randomized trees, gradient boosting, and a
-    generalized additive model (from pyGAM library, not SciKit-Learn).
-    These algorithms have been used extensively at NREL and have been
-    found to provide robust results for a range of turbine and wind plant power
-    analyses.
+    These include extremely randomized trees, gradient boosting, and a generalized additive model
+    (from pyGAM library, not SciKit-Learn). These algorithms have been used extensively at NREL and
+    have been found to provide robust results for a range of turbine and wind plant power analyses.
 
     2. Hyperparameter ranges for optimizing algorithms
 
-    Specific hyperparameter ranges for each algorithm are provided in this
-    toolkit. Similar to the learning algorithms, the hyperparameter ranges
-    are based on NREL's experience in applying these algorithms to several
-    turbine and wind plant energy analyses.
+    Specific hyperparameter ranges for each algorithm are provided in this toolkit. Similar to the
+    learning algorithms, the hyperparameter ranges are based on NREL's experience in applying these
+    algorithms to several turbine and wind plant energy analyses.
 
     3. The type of grid search used for cross-validation
 
     Two main types of grid searches are generally used:
-        exhaustive: in which all possible combinations of hyperparameters are
-                     considered, and:
-        randomized: in which random combinations are chosen and capped at a
-                    specified level
+        exhaustive: in which all possible combinations of hyperparameters are considered, and:
+        randomized: in which random combinations are chosen and capped at a specified level
 
-    In this toolkit, we implement randomized grid search only, due to the number
-    of hyperparameters and the magnitude of ranges for certain algorithms. We set
-    as default 20 randomized samples, although this can be customized within the call
-    of the hyper_optimize function.
+    In this toolkit, we implement randomized grid search only, due to the number of hyperparameters
+    and the magnitude of ranges for certain algorithms. We set as default 20 randomized samples,
+    although this can be customized within the call of the hyper_optimize function.
 
     4. The train-test split
 
-    Hyperparameter optimization is performed through cross-validation of the
-    feature and response data. By default, we assume k-fold cross-validation (i.e.
-    data is randomly partitioned into 'k' equal-sized subsamples without reordering).
-    The default 'k' value used in this toolkit is 5 (i.e. 80%/20% train-test split).
-    Both the type of cross-validation and 'k' value can be customized within the call
-    of the hyper_optimize function.
+    Hyperparameter optimization is performed through cross-validation of the feature and response
+    data. By default, we assume k-fold cross-validation (i.e. data is randomly partitioned into 'k'
+    equal-sized subsamples without reordering). The default 'k' value used in this toolkit is 5
+    (i.e. 80%/20% train-test split). Both the type of cross-validation and 'k' value can be
+    customized within the call of the hyper_optimize function.
 
     5. Model performance score
 
-    When optimzing hyperparamters, model performance is assessed based on the
-    coefficient of determination, or R2. The scorer can also be customized (e.g. RMSE)
-    within the call of the hyper_optimize function.
-
+    When optimzing hyperparamters, model performance is assessed based on the coefficient of
+    determination, or R2. The scorer can also be customized (e.g. RMSE) within the call of the
+    hyper_optimize function.
 """
 
+from __future__ import annotations
 
-class MachineLearningSetup(object):
-    def __init__(self, algorithm, params=None):
-        """
-        Initialize the class with a list of possible algorithms and
-        recommended hyperparameter ranges
-        """
-        if algorithm == "etr":  # Extra trees regressor
-            from sklearn.ensemble import ExtraTreesRegressor
+from typing import Any
 
+import attrs
+import numpy as np
+from attrs import field, define
+from pygam import GAM
+from sklearn.metrics import r2_score, make_scorer
+from sklearn.ensemble import ExtraTreesRegressor, GradientBoostingRegressor
+from sklearn.model_selection import KFold
+
+
+def _algorithm_map(abbreviation: str) -> GAM | ExtraTreesRegressor | GradientBoostingRegressor:
+    """Maps the input string abbreviation to an instantiated ML model.
+
+    Args:
+        abbreviation (str): One of "etr", "gbm", or "gam", which maps to one of
+            :py:class:`sklearn.ensemble.ExtraTreesRegressor`,
+            :py:class:`sklearn.ensemble.GradientBoostingRegressor`, or :py:class:`pygam.GAM`,
+            respectively.
+
+    Returns:
+        GAM | ExtraTreesRegressor | GradientBoostingRegressor: The actual model.
+    """
+    if abbreviation == "etr":
+        return ExtraTreesRegressor()
+    if abbreviation == "gbm":
+        return GradientBoostingRegressor()
+    if abbreviation == "gam":
+        return GAM()
+    raise NotImplementedError(
+        f"The input algorithm: {abbreviation} is either not implemented or the abbreviation is incorrect."
+    )
+
+
+@define(auto_attribs=True)
+class MachineLearningSetup:
+    algorithm: str = field(converters=(str.lower, _algorithm_map))
+    params: dict = field(default={})
+    hyper_range: dict = field(default={})
+    my_scorer: Any = field(init=False)  # Not able to be user set
+
+    def __attrs_post_init__(self):
+        """
+        Initialize the hyperparameter ranges and scorer object
+        """
+        if isinstance(self.algorithm, ExtraTreesRegressor):
             self.hyper_range = {
                 "max_depth": [4, 8, 12, 16, 20],
                 "min_samples_split": np.arange(2, 11),
                 "min_samples_leaf": np.arange(1, 11),
                 "n_estimators": np.arange(10, 801, 40),
             }
-            self.algorithm = ExtraTreesRegressor()
-
-        elif algorithm == "gbm":  # Gradient boosting model
-            from sklearn.ensemble import GradientBoostingRegressor
-
+        elif isinstance(self.algorithm, GradientBoostingRegressor):
             self.hyper_range = {
                 "max_depth": [4, 8, 12, 16, 20],
                 "min_samples_split": np.arange(2, 11),
                 "min_samples_leaf": np.arange(1, 11),
                 "n_estimators": np.arange(10, 801, 40),
             }
-            self.algorithm = GradientBoostingRegressor()
-
-        elif algorithm == "gam":  # Generalized additive model
-            from pygam import GAM
-
+        elif isinstance(self.algorithm, GAM):
             self.hyper_range = {"n_splines": np.arange(5, 40)}
-            self.algorithm = GAM()
 
         # Set scorer as R2
         self.my_scorer = make_scorer(r2_score, greater_is_better=True)
