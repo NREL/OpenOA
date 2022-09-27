a1ae373fdd4f234c09c93108a93ce37ab272a87d
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2022-09-27 16:58:22 -0600

    convert IEC to new API

--- openoa/utils/power_curve/functions.py
+++ openoa/utils/power_curve/functions.py
@@ -1,28 +1,42 @@
+"""
+This module holds ready-to-use power curve functions. They take windspeed and power columns as arguments and return a
+python function which can be used to evaluate the power curve at arbitrary locations.
+"""
+from __future__ import annotations
+
+from typing import Callable
+
 import numpy as np
 import pandas as pd
 from pygam import LinearGAM
 from scipy.optimize import differential_evolution
 
-from .parametric_forms import logistic5param
-from .parametric_optimize import least_squares, fit_parametric_power_curve
+from openoa.utils._converters import series_method
+from openoa.utils.power_curve.parametric_forms import logistic5param
+from openoa.utils.power_curve.parametric_optimize import least_squares, fit_parametric_power_curve
 
 
-"""
-This module holds ready-to-use power curve functions. They take windspeed and power columns as arguments and return a
-python function which can be used to evaluate the power curve at arbitrary locations.
-"""
-
-
-def IEC(windspeed_column, power_column, bin_width=0.5, windspeed_start=0, windspeed_end=30.0):
+@series_method(data_cols=["windspeed_col", "power_col"])
+def IEC(
+    windspeed_col: str | pd.Series,
+    power_col: str | pd.Series,
+    bin_width: float = 0.5,
+    windspeed_start: float = 0,
+    windspeed_end: float = 30.0,
+    data: pd.DataFrame = None,
+) -> Callable:
     """
-    Use IEC 61400-12-1-2 method for creating wind-speed binned power curve. Power is set to zero for windspeed values outside of the cutoff range specified by windspeed_start and windspeed_end, inclusive of the endpoints.
+    Use IEC 61400-12-1-2 method for creating a binned wind-speed power curve. Power is set to zero
+    for values outside the cutoff range: [:py:attr:`windspeed_start`, :py:attr:`windspeed_end`].
 
     Args:
-        windspeed_column (:obj:`pandas.Series`): feature column
-        power_column (:obj:`pandas.Series`): response column
-        bin_width(:obj:`float`): width of windspeed bin, default is 0.5 m/s according to standard
-        windspeed_start(:obj:`float`): left edge of first windspeed bin
-        windspeed_end(:obj:`float`): right edge of last windspeed bin
+        windspeed_column(:obj:`str` | `pandas.Series`): Windspeed data, or the name of the column in
+            :py:attr:`data`.
+        power_column(:obj:`str` | `pandas.Series`): Power data, or the name of the column in
+            :py:attr:`data`.
+        bin_width(:obj:`float`): Width of windspeed bin. Defaults to 0.5 m/s, per the standard.
+        windspeed_start(:obj:`float`): Left edge of first windspeed bin. Defaults to 0.0.
+        windspeed_end(:obj:`float`): Right edge of last windspeed bin. Defaults to 30.0
 
     Returns:
         :obj:`function`: Python function of type (Array[float] -> Array[float]) implementing the power curve.
@@ -38,8 +52,8 @@ def IEC(windspeed_column, power_column, bin_width=0.5, windspeed_start=0, windsp
 
     # Compute the mean of each bin and set corresponding P_bin
     for ibin in range(0, len(bins) - 1):
-        indices = (windspeed_column >= bins[ibin]) & (windspeed_column < bins[ibin + 1])
-        P_bin[ibin] = power_column.loc[indices].mean()
+        indices = (windspeed_col >= bins[ibin]) & (windspeed_col < bins[ibin + 1])
+        P_bin[ibin] = power_col.loc[indices].mean()
 
     # Linearly interpolate any missing bins
     P_bin = pd.Series(data=P_bin).interpolate(method="linear").bfill().values
@@ -57,7 +71,10 @@ def IEC(windspeed_column, power_column, bin_width=0.5, windspeed_start=0, windsp
     return pc_iec
 
 
-def logistic_5_parametric(windspeed_column, power_column):
+@series_method(data_cols=["windspeed_col", "power_col"])
+def logistic_5_parametric(
+    windspeed_col: str | pd.Series, power_col: str | pd.Series, data: pd.DataFrame = None
+) -> Callable:
     """
     The present implementation follows the filtering method reported in:
 
