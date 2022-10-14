2d31c9506bf2e24b210a00f4a0e6e640362ef24f
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2022-10-14 15:46:11 -0600

    change version number to 3.0rc1

--- openoa/__init__.py
+++ openoa/__init__.py
@@ -1,12 +1,13 @@
-__version__ = "3.0.0"
+__version__ = "3.0rc1"
 """
 When bumping version, please be sure to also update parameters in sphinx/conf.py
 """
 
 
-## API Shortcuts
+# API Shortcuts
 
-from openoa.plant import PlantData
 import openoa.analysis
+from openoa.plant import PlantData
+
 
 PlantData.MonteCarloAEP = openoa.analysis.aep.MonteCarloAEP
