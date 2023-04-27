c3ddb0c34e52845a66d3828c46d12b5351960ff5
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2023-04-27 15:23:21 -0600

    Attach analysis classes to `PlantData` (#221)
    * change version number to 3.0rc1
    
    * add option to use pre-cleansed data
    
    * add the analysis classes to PlantData and start blueprint for method results calculation
    
    * bumpy scikit-learn version
    
    * bumpy scipy version
    
    * add analysis class attachment logic
    
    * update changelog
    
    * remove hard-coded abbreviation checks
    
    * update final notebook for new analysis creation patterns
    
    * add required argument
    
    * remove accidental addition
    
    * rollback version changes

--- openoa/__init__.py
+++ openoa/__init__.py
@@ -3,10 +3,23 @@ __version__ = "3.0rc2"
 When bumping version, please be sure to also update parameters in sphinx/conf.py
 """
 
-
-# API Shortcuts
-
 from openoa.plant import PlantData
 
 
-# TODO: Attach analysis classes to PlantData
+def __attach_methods():
+    from openoa.analysis.aep import create_MonteCarloAEP
+    from openoa.analysis.wake_losses import create_WakeLosses
+    from openoa.analysis.eya_gap_analysis import create_EYAGapAnalysis
+    from openoa.analysis.electrical_losses import create_ElectricalLosses
+    from openoa.analysis.turbine_long_term_gross_energy import create_TurbineLongTermGrossEnergy
+
+    setattr(PlantData, "MonteCarloAEP", create_MonteCarloAEP)
+    setattr(PlantData, "EYAGapAnalysis", create_EYAGapAnalysis)
+    setattr(PlantData, "ElectricalLosses", create_ElectricalLosses)
+    setattr(PlantData, "TurbineLongTermGrossEnergy", create_TurbineLongTermGrossEnergy)
+    setattr(PlantData, "WakeLosses", create_WakeLosses)
+
+
+__attach_methods()
+
+# TODO: Add analysis results computation methods to PlantData
