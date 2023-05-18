d16909607031af75fb6c72a19f35701ddad23026
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2023-05-18 15:50:22 -0600

    add schema generation and sample files

---
+++ openoa/schema/schema.py
@@ -0,0 +1,96 @@
+"""Methods to generate YAML and JSON schema files"""
+
+import json
+from typing import Any
+from pathlib import Path
+
+import yaml
+from attrs import Attribute, asdict
+
+from openoa.plant import (
+    AssetMetaData,
+    MeterMetaData,
+    SCADAMetaData,
+    TowerMetaData,
+    StatusMetaData,
+    CurtailMetaData,
+    ReanalysisMetaData,
+)
+
+
+HERE = Path(__file__).resolve().parent
+
+
+def _attrs_meta_filter(inst: Attribute, value: Any) -> bool:
+    """Filters out any unnecessary components of the metadata classes.
+
+    Args:
+        inst (Attribute): The class attribute (``attrs.field``) being checked
+        value (Any): The actual values in the field.
+
+    Returns:
+        bool: False, if should not be serialized, and True, if it should be serialized.
+    """
+    if inst.name in ("col_map", "name", "col_map_reversed"):
+        return False
+    if inst is None or value is None:
+        return False
+    return True
+
+
+def _attrs_meta_serializer(inst: type, field: Attribute, value: Any) -> Any:
+    """Custom serialization for attrs dataclass fields.
+
+    Args:
+        inst (type): The class object.
+        field (Attribute): The ``attrs.field`` information.
+        value (Any): The actual values in the :py:attr:`field`.
+
+    Returns:
+        Any: Reformatted data.
+    """
+    if field is None:
+        return value
+    if field.name == "dtypes":
+        value = {k: str(v).replace("<class '", "").replace("'>", "") for k, v in value.items()}
+        return value
+    return value
+
+
+def create_schema() -> dict:
+    """Creates a dictionary of the metadata input requirements.
+
+    Returns:
+        dict: The compiled metadata dictionary specifying the required data definitions.
+    """
+    meta_classes = (
+        SCADAMetaData,
+        MeterMetaData,
+        TowerMetaData,
+        StatusMetaData,
+        CurtailMetaData,
+        AssetMetaData,
+        ReanalysisMetaData,
+    )
+    schema = {}
+    for meta in meta_classes:
+        cls = meta()
+        meta_dict = asdict(cls, filter=_attrs_meta_filter, value_serializer=_attrs_meta_serializer)
+        schema[cls.name] = {"""broken out by column into name, dtype, and units"""}
+    return schema
+
+
+# TODO: bring metadata components of plant.py into schema/
+# TODO: replace ID naming to asset_id
+
+
+if __name__ == "__main__":
+    # Get the schemas
+    full_schema = create_schema()
+
+    # Save the analysis schemass
+    with open(HERE / "full_schema.yml", "w") as f:
+        yaml.dump(full_schema, f, default_flow_style=False, sort_keys=False)
+
+    with open(HERE / "full_schema.json", "w") as f:
+        json.dump(full_schema, f, sort_keys=False, indent=2)
