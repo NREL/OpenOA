4f144493b4f727737689bf0ca4bf05228106ed17
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2023-08-30 15:27:34 -0700

    remove prints

--- test/regression/yaw_misalignment.py
+++ test/regression/yaw_misalignment.py
@@ -286,15 +286,8 @@ class TestStaticYawMisalignment(unittest.TestCase):
 
         calculated_yaw_mis_results_95ci_ws = self.analysis.yaw_misalignment_95ci_ws
 
-        print("expected")
-        print(expected_yaw_mis_results_95ci_ws)
-        print()
-        print("actual")
-        print(calculated_yaw_mis_results_95ci_ws)
         nptest.assert_array_almost_equal(
-            expected_yaw_mis_results_95ci_ws,
-            calculated_yaw_mis_results_95ci_ws,
-            decimal=5,
+            expected_yaw_mis_results_95ci_ws, calculated_yaw_mis_results_95ci_ws, decimal=5
         )
 
     def check_simulation_results_yaw_misalignment_with_UQ_new_params(self):
