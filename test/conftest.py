7413b6fc16519cac691a3158a94be28f69e29ca7
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2023-12-06 11:59:05 -0800

    add easier way to run unit and/or regression

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,37 @@
 import sys
 from pathlib import Path
 
+import pytest
+
 
 examples_folder = Path(__file__).resolve().parents[1]
 sys.path.append(examples_folder)
 from examples import project_ENGIE, example_data_path_str  # noqa: disable=E402
+
+
+ROOT = Path(__file__).parent
+
+
+def pytest_addoption(parser):
+    parser.addoption("--unit", action="store_true", default=False, help="run tests in test/unit/.")
+    parser.addoption(
+        "--regression", action="store_true", default=False, help="run tests in test/regression/."
+    )
+
+
+def pytest_configure(config):
+    unit = config.getoption("--unit")
+    regression = config.getoption("--regression")
+
+    if regression:
+        regression_tests = list((ROOT / "regression").iterdir())
+
+    if unit:
+        unit_tests = list((ROOT / "unit").iterdir())
+
+    if regression and unit:
+        config.args = unit_tests + regression_tests
+    elif regression:
+        config.args = regression_tests
+    else:
+        config.args = unit_tests
